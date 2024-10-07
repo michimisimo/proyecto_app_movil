f28f943a94d583928f41e7fa0f2980451f54f4af
mariajosecq <cote21cq@gmail.com>
2024-10-07 03:22:32 -0300

    Se eliminan validaciones de la página de registro

--- src/app/page/registro/registro.page.ts
+++ src/app/page/registro/registro.page.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { Injectable } from '@angular/core';
 import { ErrorPerfilUsuario } from 'src/app/models/error-perfil-usuario';
+import { validarPerfilUsuario} from 'src/app/utils/validacion/valid-registro';
 
 @Injectable({
   providedIn: 'root',
@@ -53,7 +54,7 @@ export class RegistroPage {
     this.passwordConfirm = "";
   }
 
-  //Se crea el popup de error para la validación de campos
+  //Crear el popup de error para la validación de campos
   private async showAlert(header: string, message: string) {
     const alert = await this.alertController.create({
       header,
@@ -63,10 +64,8 @@ export class RegistroPage {
     await alert.present();
   }
 
-
-  validarPerfilUsuario = (perfilUsuario: PerfilUsuario) => {
-    const error: any = {}; // Reiniciar errores antes de la validación
-
+  registrar(perfilUsuario: PerfilUsuario) {
+    
     // Selección de inputs por nombre
     const inputUsuario = document.getElementsByName('username')[0] as HTMLInputElement;
     const inputPassword = document.getElementsByName('password')[0] as HTMLInputElement;
@@ -76,94 +75,21 @@ export class RegistroPage {
     const inputCorreo = document.getElementsByName('email')[0] as HTMLInputElement;
     const inputTelefono = document.getElementsByName('phone')[0] as HTMLInputElement;
 
-    // Función para aplicar o quitar clases de error
-    const aplicarEstilos = (input: HTMLInputElement, errorMsg: string, esError: boolean) => {
-      if (esError) {
-        input.classList.add('borde_rojo');
-        input.classList.remove('sin_borde');
-      } else {
-        input.classList.add('sin_borde');
-        input.classList.remove('borde_rojo');
-      }
-      // Para el placeholder
-      //input.placeholder = esError ? errorMsg : '';
-    };
-
-    // Verifica longitud del nombre de usuario
-    if (perfilUsuario.user.usuario.length < 4 || perfilUsuario.user.usuario.length > 20) {
-      error.usuario = "Ingrese un usuario de 4 a 20 caracteres";
-      aplicarEstilos(inputUsuario, error.usuario, true);
-    } else {
-      aplicarEstilos(inputUsuario, '', false);
-    }
-
-    // Verifica si el nombre está vacío
-    if (perfilUsuario.nombre === "") {
-      error.nombre = "Ingrese su nombre";
-      aplicarEstilos(inputNombre, error.nombre, true);
-    } else {
-      aplicarEstilos(inputNombre, '', false);
-    }
-
-    // Verifica si el apellido está vacío
-    if (perfilUsuario.apellido === "") {
-      error.apellido = "Ingrese su apellido";
-      aplicarEstilos(inputApellido, error.apellido, true);
-    } else {
-      aplicarEstilos(inputApellido, '', false);
-    }
-
-    // Verifica la longitud mínima de la contraseña
-    if (perfilUsuario.user.password.length < 8) {
-      error.password = "La contraseña debe tener al menos 8 caracteres";
-      aplicarEstilos(inputPassword, error.password, true);
-      // La contraseña de confirmación debe también tener borde rojo y placeholder vacío
-      aplicarEstilos(inputPassConfirm, '', true);
-    } else if (perfilUsuario.user.password !== this.passwordConfirm) {
-      // Verifica coincidencia de contraseñas
-      error.password_match = "Las contraseñas no coinciden.";
-      aplicarEstilos(inputPassword, '', false);
-      aplicarEstilos(inputPassConfirm, error.password_match, true);
-    } else {
-      aplicarEstilos(inputPassword, '', false);
-      aplicarEstilos(inputPassConfirm, '', false);
-    }
-
-    // Verifica formato del correo electrónico
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(perfilUsuario.correo)) {
-      error.correo = "Correo electrónico no válido.";
-      aplicarEstilos(inputCorreo, error.correo, true);
-    } else {
-      aplicarEstilos(inputCorreo, '', false);
-    }
-
-    // Verifica que el teléfono contenga solo caracteres numéricos y tenga exactamente 9 dígitos
-    if (perfilUsuario.telefono.length !== 9) {
-      error.telefono = "El teléfono debe contener 9 dígitos";
-      aplicarEstilos(inputTelefono, error.telefono, true);
-    } else {
-      aplicarEstilos(inputTelefono, '', false);
-    }
-
-    return error;
-  };
-
-
-  registrar(perfilUsuario: PerfilUsuario) {
-    // Actualiza errores
-    this.error = this.validarPerfilUsuario(this.perfilUsuario);
+    // Obtener error
+    this.error = validarPerfilUsuario(this.perfilUsuario, this.passwordConfirm, inputUsuario, inputPassword, inputPassConfirm, inputNombre, inputApellido, inputCorreo, inputTelefono);
+    
     if (Object.keys(this.error).length > 0) {
       console.log("Errores encontrados:", this.error);
       this.showAlert('ERROR', 'Por favor, corrige los errores antes de continuar.');
       return;
     }
 
-    // Encripta la contraseña
+    // Encriptar la contraseña
     const hashedPassword = this.authService.encryptPassword(this.perfilUsuario.user.password);
     console.log('Contraseña encriptada:', hashedPassword);
     this.perfilUsuario.user.password = hashedPassword;
 
-    // Agrega el usuario a la lista de usuarios
+    // Agregar el usuario a la lista de usuarios
     this._userService.agregar_usuario(this.perfilUsuario);
     this.router.navigate(['login']);
   }
@@ -172,4 +98,3 @@ export class RegistroPage {
     this.router.navigate(['login']);
   }
 }
-
