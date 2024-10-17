66588cae6d80673a0b55309a10373c04ac109d47
mariajosecq <cote21cq@gmail.com>
2024-10-17 03:10:18 -0300

    Se crea lógica para mostrar lista de contactos del usuario logueado

--- src/app/page/contactos/contactos.page.ts
+++ src/app/page/contactos/contactos.page.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ServiceAmigosService } from 'src/app/api/service_amigos/service-amigos.service';
 import { ServiceUserService } from 'src/app/api/service_user/service-user.service';
+import { ServiceUsuarioService } from 'src/app/api/service_usuario/service-usuario.service';
 import { Amigo } from 'src/app/models/amigo';
+import { PerfilUsuario } from 'src/app/models/perfil-usuario';
 import { User } from 'src/app/models/user';
 
 @Component({
@@ -18,19 +20,36 @@ export class ContactosPage implements OnInit {
     password: ''
   };
 
+  perfilUsuario: PerfilUsuario | null = {
+    nombre: '',
+    apellido: '',
+    correo: '',
+    telefono: ''
+  }
+
   mostrarLista: string = 'todos';
 
   listaAmistades: Amigo[] = []
 
-  constructor(private _userService : ServiceUserService, private router : Router, private _amigoService : ServiceAmigosService) { }
+  listaContactos: Amigo[] = []
+
+  listaPerfiles: PerfilUsuario[] = [];
+
+  constructor(private _userService : ServiceUserService, private router : Router, private _amigoService : ServiceAmigosService, private _usuarioService : ServiceUsuarioService) { }
 
   ngOnInit() {
     //Se obtiene el user seteado en el User Service
     this._userService.user$.subscribe(user => {
       this.user = user;
-      console.log('Usuario en contactos:', this.user);
-      this.obtenerListaAmistades();
+      console.log('User en contactos:', this.user);
     });
+    //Se obtiene el usuario seteado en el Usuario Service
+    this._usuarioService.usuario$.subscribe(usuario => {
+      this.perfilUsuario = usuario;
+      console.log('Usuario en contactos:', this.perfilUsuario);
+    });
+    this.obtenerContactos();
+    
   }
 
   irHome(){
@@ -49,12 +68,23 @@ export class ContactosPage implements OnInit {
     this.mostrarLista = 'agregar-contacto';
   }
 
-  obtenerListaAmistades(){
+  obtenerContactos(){
     this._amigoService.getAmigo().subscribe({
       next: (response) => {
         if (response.body != null) {
-          this.listaAmistades = response.body;   
-          console.log(this.listaAmistades)       
+          this.listaAmistades = response.body;
+          const listaContactos = [];   
+          for (let i = 0; i < this.listaAmistades.length; i++) {
+            if(this.perfilUsuario){
+              if (this.listaAmistades[i].id_persona1 == this.perfilUsuario.id_persona){
+                listaContactos.push(this.listaAmistades[i]);
+                this.listaContactos = listaContactos;
+              }
+            }            
+          }
+          this.obtenerPerfiles(listaContactos);
+          console.log("Lista amistades"+JSON.stringify(this.listaAmistades));
+          console.log("Lista contactos"+JSON.stringify(this.listaContactos)); 
         } else {
           console.error('No se encontró la lista de contactos');
         }
@@ -65,7 +95,25 @@ export class ContactosPage implements OnInit {
     });
   }
 
-  
+  obtenerPerfiles(listaContactos:Amigo[]){
+    const listaPerfiles: PerfilUsuario[] = [];
+    for (let i = 0; i < listaContactos.length; i++) {
+      this._usuarioService.getUsuarioById(listaContactos[i].id_persona2).subscribe ({
+        next: (response) => {
+          if(response.body){
+            if (this.perfilUsuario){
+              listaPerfiles.push(response.body[0]);
+              this.listaPerfiles = listaPerfiles;            
+            }
+            
+          }else{
+            console.log("No se encontraron perfiles para la lista de contactos")
+          }
+        }        
+      });      
+    }
+    
+  }
 
   
 
