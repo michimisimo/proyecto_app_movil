df08756a73acc24f66b650155c38b9e9d4a6845e
mariajosecq <cote21cq@gmail.com>
2024-10-16 23:01:10 -0300

    Se ubica menú de navegación en app.component como base para utilizarlo en todas las pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
+import { Router } from '@angular/router';
+import { ServiceUserService } from './api/service_user/service-user.service';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  user: User | null = null;
+  
+  constructor(private router: Router, private _userService : ServiceUserService, private menu: MenuController) {}
+
+  ngOnInit() {
+    this._userService.user$.subscribe(user => {
+      this.user = user;
+      console.log('Usuario en app components:', this.user);
+    });
+  }
+
+  irVerPerfil() {
+    this.menu.close(); 
+    this.router.navigate(['perfil-usuario'])
+  }
+
+  irCrearEvento() {
+    this.menu.close(); 
+    //this.router.navigate(['/crear-evento']); 
+  }
+
+  irMisEventos() {
+    this.menu.close(); 
+    //this.router.navigate(['/mis-eventos']); 
+  }
+
+  irContactos(){
+    this.menu.close(); 
+    this.router.navigate(['contactos'])
+  }
+
+  irConfiguracion() {
+    this.menu.close(); 
+    //this.router.navigate(['/configuracion']); 
+  }
+
+  irAyuda() {
+    this.menu.close(); 
+    //this.router.navigate(['/ayuda']); 
+  }
 }
+
