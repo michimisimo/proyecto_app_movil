4cd8dd41aabd3d5624a1c5aebfe7310a33205989
Felipe Urbina <furbina@demafront.cl>
2024-10-21 17:59:24 -0300

    se mejora la forma en la que se obtiene listaInvitado y nombre creador en evento

--- src/app/page/evento/evento.page.ts
+++ src/app/page/evento/evento.page.ts
@@ -27,22 +27,22 @@ export class EventoPage implements OnInit {
     id_creador: 0
   };
 
-  foto_evento: FotoEvento ={
+  foto_evento: FotoEvento = {
     id_evento: 0,
     url_foto_evento: ''
   }
 
+  nombreCreador: string = '';
   listaEventos: Evento[] = [];
   listaFotosEvento: FotoEvento[] = [];
   listaInvitaciones: InvitacionEvento[] = [];
   listaInvitados: PerfilUsuario[] = [];
-  invitadosCargados: boolean = false; // Variable de estado para controlar la carga
 
 
   constructor(
-    private router: Router, 
-    private _eventoService: ServiceEventoService, 
-    private _perfilUsuarioService: ServicePerfilUsuarioService, 
+    private router: Router,
+    private _eventoService: ServiceEventoService,
+    private _perfilUsuarioService: ServicePerfilUsuarioService,
     private _invitacionService: ServiceInvitacionEventoService,
     private _fotoEventoService: ServiceFotoEventoService,
     private _imageService: ServiceImageService) { }
@@ -58,20 +58,14 @@ export class EventoPage implements OnInit {
     }
   }
 
-  accordionGroupChange = (id_evento: number) => {
-    if (!this.invitadosCargados) { // Verifica si los invitados ya han sido cargados
-      this.obtenerInvitados(id_evento);
-      this.invitadosCargados = true; // Marca que los invitados ya se han cargado
-      console.log('acordeon activado');
-    }
-  };
-
   obtenerEvento() {
     if (this.evento.id_evento) {
       this._eventoService.getEventoById(this.evento.id_evento).subscribe({
         next: (response) => {
           if (response.body) {
             this.evento = response.body[0];
+            this.obtenerNombreCreador(this.evento.id_creador!);
+            this.obtenerInvitados(this.evento.id_evento!)
             console.log("Evento:" + JSON.stringify(this.evento));
           }
         }
@@ -106,30 +100,30 @@ export class EventoPage implements OnInit {
 
   subirImagenes(event: Event) {
     const fileInput = event.target as HTMLInputElement;
-  
+
     if (fileInput.files) {
       const archivos = Array.from(fileInput.files); // Convierte a un array
-  
+
       archivos.forEach(archivo => {
         // Maneja la subida de cada archivo
-        if(this.evento.id_evento){
+        if (this.evento.id_evento) {
           this._imageService.uploadImage('eventos', 'evento', this.evento.id_evento, archivo).subscribe(
             (response) => {
               console.log('Imagen subida con éxito:', response);
               const url = `${environment.storage_url}object/public/eventos/evento-${this.evento.id_evento}/${archivo.name}`;
               // Aquí puedes hacer algo con la URL, como agregarla a una lista
               console.log('URL de la imagen:', url);
-              if(this.evento.id_evento){
+              if (this.evento.id_evento) {
                 this.foto_evento.id_evento = this.evento.id_evento;
                 this.foto_evento.url_foto_evento = url;
-                this._fotoEventoService.createFotoEvento(this.foto_evento).subscribe( 
+                this._fotoEventoService.createFotoEvento(this.foto_evento).subscribe(
                   (response) => {
                     console.log("Foto del evento subida con éxito")
                     this.obtenerListaFotosEvento();
                   }, error => {
                     console.error('Error al subir foto del evento', error);
                   });
-              }             
+              }
 
             },
             (error) => {
@@ -141,60 +135,27 @@ export class EventoPage implements OnInit {
       });
     }
   }
-  
 
-  eventosCache = new Map<number, string>(); // Cache para eventos y creadores
+  obtenerNombreCreador(id_creador: number) {
+    // Realiza la llamada para obtener el perfil del creador
+    this._perfilUsuarioService.getPerfilUsuarioById(id_creador).subscribe({
+      next: (Response) => {
+        const creadores: PerfilUsuario[] = Response.body || [];
+        const creador = creadores[0];
+        const nombreCompleto = `${creador.nombre} ${creador.apellido}`;
+        // Guarda en cache el resultado para evitar futuras llamadas
 
-  obtenerNombreCreador(id_evento: number): string {
-    // Revisa si el evento ya está en caché
-    if (this.eventosCache.has(id_evento)) {
-      return this.eventosCache.get(id_evento)!;
-    }
+        this.nombreCreador = nombreCompleto;
 
-    this._eventoService.getEventos().subscribe({
-      next: (Response) =>
-        this.listaEventos = (Response.body || [])
-    })
-    // Busca el evento en la lista
-    const evento = this.listaEventos.find(evento => evento.id_evento === id_evento);
-
-    if (evento) {
-      // Realiza la llamada para obtener el perfil del creador
-      this._perfilUsuarioService.getPerfilUsuarioById(evento.id_creador).subscribe({
-        next: (Response) => {
-          const creadores: PerfilUsuario[] = Response.body || [];
-          if (creadores.length > 0) {
-            const creador = creadores[0];
-            const nombreCompleto = `${creador.nombre} ${creador.apellido}`;
-
-            // Guarda en cache el resultado para evitar futuras llamadas
-            this.eventosCache.set(id_evento, nombreCompleto);
-          }
-        },
-        error: (err) => {
-          console.error('Error al obtener el creador', err);
-        }
-      });
-
-      // Devuelve un mensaje provisional mientras se espera la respuesta
-      return 'Cargando...';
-    } else {
-      return 'Evento no encontrado';
-    }
+      },
+      error: (error) => {
+        console.log('Error al obtener el creador');
+      }
+    });
   }
-
-  // Cache para almacenar los invitados de cada evento
-  invitadosCache = new Map<number, PerfilUsuario[]>(); // Mapa donde la clave es el id_evento
 
   obtenerInvitados(eventoId: number): void {
     console.log('ejecutando funcion para invitados')
-    // Revisa si ya tenemos los invitados en caché
-    if (this.invitadosCache.has(eventoId)) {
-      this.listaInvitados = this.invitadosCache.get(eventoId)!;
-      // Obtiene los invitados del caché
-      return; // Salimos de la función ya que no es necesario hacer una llamada a la API
-    }
-
     // Inicializa la lista de invitados vacía
     this.listaInvitados = [];
 
@@ -206,22 +167,15 @@ export class EventoPage implements OnInit {
 
         console.log(listaInvitaciones)
 
-        // Usamos Promise.all para esperar todas las consultas a los perfiles de los invitados
-        const perfilPromises = listaInvitaciones.map(invitacion =>
-          this._perfilUsuarioService.getPerfilUsuarioById(invitacion.id_invitado).toPromise()
+        listaInvitaciones.forEach(invitacion =>
+          this._perfilUsuarioService.getPerfilUsuarioById(invitacion.id_invitado).subscribe({
+            next: (Response) => {
+              const invitado: PerfilUsuario = (Response.body![0])
+              this.listaInvitados.push(invitado)
+            }
+          })
         );
 
-        // Cuando todas las promesas se resuelven, llenamos la lista de invitados
-        Promise.all(perfilPromises).then((perfiles) => {
-          this.listaInvitados = perfiles
-            .map(Response => Response?.body ? Response.body[0] : null) // Asigna el perfil o null si no existe
-            .filter(invitado => invitado !== null) as PerfilUsuario[]; // Filtra los nulos y asegura que sea del tipo correcto
-
-          // Guarda los invitados en caché
-          this.invitadosCache.set(eventoId, this.listaInvitados);
-        }).catch(err => {
-          console.error('Error al obtener perfiles de invitados', err);
-        });
       },
       error: (err) => {
         console.error('Error al obtener invitaciones del evento', err);
@@ -237,6 +191,6 @@ export class EventoPage implements OnInit {
     this.router.navigate(['galeria-evento'], {
       state: { idEvento: this.evento.id_evento }
     });
-}
+  }
 
 }
