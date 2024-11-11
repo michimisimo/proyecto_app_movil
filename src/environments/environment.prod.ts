8078718f75342da99aea068c47180b9327a75e8a
mariajosecq <cote21cq@gmail.com>
2024-11-11 15:46:07 -0300

    Se crea carpeta android y se añaden constantes a environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
 export const environment = {
-  production: true
+  production: true,
+  api_url: "https://zkfhrkmxpowygtruytmr.supabase.co/rest/v1/",
+  apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZmhya214cG93eWd0cnV5dG1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxMzMwMzEsImV4cCI6MjA0MjcwOTAzMX0.0-0VWZno6ggciL5u5aK-mHllB5bDueaHGCZXtosTX9k",
+  supabase: createClient("https://zkfhrkmxpowygtruytmr.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZmhya214cG93eWd0cnV5dG1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxMzMwMzEsImV4cCI6MjA0MjcwOTAzMX0.0-0VWZno6ggciL5u5aK-mHllB5bDueaHGCZXtosTX9k"),
+  storage_url: "https://zkfhrkmxpowygtruytmr.supabase.co/storage/v1/"
 };
