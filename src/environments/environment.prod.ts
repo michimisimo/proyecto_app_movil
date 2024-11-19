4b6eabbbc94775c42c6c808038fc38f93a384f53
Felipe Urbina <furbina@demafront.cl>
2024-11-19 16:11:27 -0300

    se vuelve a crear la carpeta android con el build y se corrigue el enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   api_url: "https://zkfhrkmxpowygtruytmr.supabase.co/rest/v1/",
   apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZmhya214cG93eWd0cnV5dG1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxMzMwMzEsImV4cCI6MjA0MjcwOTAzMX0.0-0VWZno6ggciL5u5aK-mHllB5bDueaHGCZXtosTX9k",
   supabase: createClient("https://zkfhrkmxpowygtruytmr.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZmhya214cG93eWd0cnV5dG1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxMzMwMzEsImV4cCI6MjA0MjcwOTAzMX0.0-0VWZno6ggciL5u5aK-mHllB5bDueaHGCZXtosTX9k"),
-  storage_url: "https://zkfhrkmxpowygtruytmr.supabase.co/storage/v1/"
+  storage_url: "https://zkfhrkmxpowygtruytmr.supabase.co/storage/v1/",
+  mapbox_Key: "pk.eyJ1IjoibWljaGltaXNpbW8xIiwiYSI6ImNtM2djdjB3djAzNTIyanBxNjZoNG50cWUifQ.FW47IH33nO7jrRAGdQL6eQ"
 };
